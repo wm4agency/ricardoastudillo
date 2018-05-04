6614676d7efaa7cf6f1d026f01ee265576f7ba7a
monowm4 <oscar@wm4.mx>
2018-05-04 11:11:53 -0500

    hero header section styles
    -%textImgSwap sass placeholder grouping format for text elements that will be swaped by an image in the viewport
    -"logos" div added to the structure to provide a common wrapper to provide common frame to elements that needed to be position relative to each other
    - further styling of elements within section

--- p/partials/hero.php
+++ p/partials/hero.php
@@ -4,14 +4,16 @@
     <h2>candidato a presidente municipal</h2>
     <h3>municipio de Corregidora, Querétaro</h3>
   </hgroup>
-  <figure class="corregidora">
-    <hgroup>
-      <h3>Corregidora</h3>
-      <h4>¡Posible!</h4>
-    </hgroup>
-  </figure>
-  <div class="coalicion">
-    <figure class="pri"></figure>
-    <figure class="verde"></figure>
+  <div class="logos">
+    <figure class="corregidora">
+      <hgroup>
+        <h3>Corregidora</h3>
+        <h4>¡Posible!</h4>
+      </hgroup>
+    </figure>
+    <div class="coalicion">
+      <figure class="pri"><h5>Partido de la Revolución Democrática</h5></figure>
+      <figure class="verde"><h5>Partido Verde Ecologista de México</h5></figure>
+    </div>
   </div>
 </div>
