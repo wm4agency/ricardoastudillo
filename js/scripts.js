0cdb7f1f7478e46fa4a8556af4cbbd1893b91aab
monowm4 <oscar@wm4.mx>
2018-05-08 10:39:40 -0500

    preventDefault commented out
    let default actions triggered to avoid things like inputs not be usable

--- js/scripts.js
+++ js/scripts.js
@@ -22,46 +22,33 @@ function delegateEvents(){
     //port touch events to click events
     document.addEventListener('touchend', function(e) {
       /* prevent delay and simulated mouse events */
-      e.stopPropagation();
-      e.preventDefault();
+      //e.stopPropagation();
+      //e.preventDefault();
       /* trigger the actual behavior we bound to the 'click' event */
       e.target.click();
     })
     document.addEventListener('click', function (event) {
         var e = event.target;
-        e.preventDefault
+        //e.preventDefault
+        //console.log(e);
 
         if (e.classList.contains( "m-nav-toggler" ) ) { // botones en formas de contacto
+            e.preventDefault
             togglenav();
         }
         else if (e.classList.contains( 'sendform' ) ) { //envíos de formas
+            e.preventDefault
             var forma = e.form;
             checkvals(forma);
         }
         else if(e.getAttribute("data-target")){// objetos que disparan navegación
+            e.preventDefault
             navegar(e);
         }
 
     }, false);
 }
-// function delegateEventsMobile(){
-//   document.addEventListener('touchstart', function (event) {
-//       var e = event.target;
-//       e.preventDefault
-//
-//       if (e.classList.contains( "m-nav-toggler" ) ) { // botones en formas de contacto
-//           togglenav();
-//       }
-//       else if (e.classList.contains( 'sendform' ) ) { //envíos de formas
-//           var forma = e.form;
-//           checkvals(forma);
-//       }
-//       else if(e.getAttribute("data-target")){// objetos que disparan navegación
-//           navegar(e);
-//       }
-//
-//   }, false);
-// }
+
 
 function togglenav(){
     var panel = document.getElementById('panel');
