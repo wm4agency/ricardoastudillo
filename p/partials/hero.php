cb4e85dd1c831cd3d6348119269d3a0f5116bea3
monowm4 <oscar@wm4.mx>
2018-05-11 18:04:57 -0500

    visual id mayor update- logos, menu and hero section

--- p/partials/hero.php
+++ p/partials/hero.php
@@ -1,19 +1,24 @@
 <div class="container">
-  <hgroup class="astudillo">
-    <h1>Ricardo Astudillo</h1>
-    <h2>candidato a presidente municipal</h2>
-    <h3>municipio de Corregidora, Querétaro</h3>
-  </hgroup>
+  <figure class="corregidora">
+    <hgroup>
+      <h3>Corregidora</h3>
+      <h4>¡Posible!</h4>
+    </hgroup>
+  </figure>
   <div class="logos">
-    <figure class="corregidora">
-      <hgroup>
-        <h3>Corregidora</h3>
-        <h4>¡Posible!</h4>
-      </hgroup>
-    </figure>
+    <hgroup class="astudillo">
+      <h1>Ricardo Astudillo</h1>
+      <h2>candidato a presidente municipal</h2>
+      <h3>municipio de Corregidora, Querétaro</h3>
+    </hgroup>
     <div class="coalicion">
-      <figure class="pri"><h5>Partido de la Revolución Democrática</h5></figure>
-      <figure class="verde"><h5>Partido Verde Ecologista de México</h5></figure>
+      <div class="partidoswrapp">
+        <figure class="pri"><h5>Partido de la Revolución Democrática</h5></figure>
+        <figure class="verde"><h5>Partido Verde Ecologista de México</h5></figure>
+      </div>
+      <div class="flechaswrapp">
+        <figure class="flechas_coalicion"></figure>
+      </div>
     </div>
   </div>
 </div>
