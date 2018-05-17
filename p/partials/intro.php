1495f7b50c4f5cc690477158c83a94c7a985a320
monowm4 <oscar@wm4.mx>
2018-05-16 19:10:55 -0500

    new quote and quote styling

--- p/partials/intro.php
+++ p/partials/intro.php
@@ -8,6 +8,12 @@
 </main>
 <blockquote>
   <div class="container">
-    <p>"Un corregidora seguro,<br /> lleno de oportunidades,<br /> para disfrutar y prosperar<br /> todos juntos con equidad"</p>
+    <p>"Hemos comenzado un nuevo capítulo para CORREGIDORA,<br /> tenemos que poner el corazón por delante.<br>
+Podemos tener un CORREGIDORA POSIBLE<br> y este es el momento para hacerlo.<br>
+Vamos por el CORREGIDORA que hemos soñado.<br>
+Vamos por el CORREGIDORA que los habitantes merecen,<br> lo vamos a hacer y que venga el mejor tiempo."</p>
+<footer><cite>Ricardo Astudillo.</cite></footer>
+
+
   </div>
 </blockquote>
