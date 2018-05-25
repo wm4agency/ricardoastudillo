374a85f388034c44a44a187d80b2904c30333b32
monowm4 <oscar@wm4.mx>
2018-05-25 09:51:33 -0500

    typos intro

--- p/partials/intro.php
+++ p/partials/intro.php
@@ -2,9 +2,7 @@
 <main>
   <div class="container">
     <p>
-      Soy Ricardo Astudillo, esposo de Ma. del Carmen Urquiza González Cosio "Chocho" y papá de Sofía, Ricardo y Mariano. Mis padres siempre me impulsaron a construir mis metas y con es a visión, decidí ser arquitecto.</p>
-
-<p>En mi trayectoria he sido Regidor del Municipio de Corregidora (2006 - 2009), Diputado Local al Congreso de Querétaro (2009 - 2012), y Diputado Federal por el distrito 2 de Querétaro. En el congreso, ocupé los cargos de Secretario de la Comisión de Energía e integrante de las comisiones de Medio Ambiente y Recursos Naturales y de Presupuesto y Cuenta Pública y en la Comisión Especial de Casos de Corrupción Ocurridos en Petróleos Mexicanos.
+      Soy Ricardo Astudillo, esposo de Ma. del Carmen Urquiza González Cosio "Chocho" y papá de Sofía, Ricardo y Mariano. Mis padres siempre me impulsaron a construir mis metas y con esa visión, decidí ser arquitecto.<br />En mi trayectoria he sido Regidor del Municipio de Corregidora (2006 - 2009), Diputado Local al Congreso de Querétaro (2009 - 2012), y Diputado Federal por el distrito 2 de Querétaro. En el congreso, ocupé los cargos de Secretario de la Comisión de Energía e integrante de las comisiones de Medio Ambiente y Recursos Naturales y de Presupuesto y Cuenta Pública y en la Comisión Especial de Casos de Corrupción Ocurridos en Petróleos Mexicanos.
     </p>
   </div>
 </main>
